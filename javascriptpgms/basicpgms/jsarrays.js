48c608ecd326e39ae7ac7c205acf14e6b5ad48ee
krishna <krisha181920@gmail.com>
2021-04-09 12:32:42 +0530

    myinit

--- javascriptpgms/basicpgms/jsarrays.js
+++ javascriptpgms/basicpgms/jsarrays.js
@@ -1,8 +1,32 @@
 var arr=[10,11,12,13,14,15,17,18,22,24]
-console.log(`length of array is ${}`);
+var lenofarr= arr.length
+console.log(`length of array is ${lenofarr}`);
 //print all even number
+// for(let num of arr){
+//     if(num%2==0){
+//         console.log(num);
+//     }
+// }
+
+//odd and even array
+var oddarr=[]
+var evenarr=[]
 for(let num of arr){
     if(num%2==0){
-        console.log(num);
+        evenarr.push(num);
+    }
+    else{
+        oddarr.push(num)
     }
 }
+console.log(`even elemets`);
+console.log(evenarr);
+// for(let element of evenarr){
+
+//     console.log(` ${element}`);
+// }
+console.log(`odd elements`);
+console.log(oddarr);
+// for(let element of oddarr){
+//     console.log(` ${element}`);
+// }
